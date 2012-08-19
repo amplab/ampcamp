2b89e9f5b83c3b259745110259b9726ed5a9787d
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2012-08-18 19:10:05 -0700

    First cut of script to launch many clusters in parallel. Not tested yet

---
+++ launch_ampcamp_cluster.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import subprocess
+import sys
+
+from optparse import OptionParser
+from sys import stderr
+
+def parse_args():
+  parser = OptionParser(usage="launch_ampcamp_cluster [options] <path_to_spark_ec2.py>",
+      add_help_option=True)
+  parser.add_option("-n", "--clusters", type="int", default=1,
+      help="Number of clusters to launch (default: 1)")
+  parser.add_option("-b", "--buckets", type="int", default=1,
+      help="number of s3 bucket clones (default: 1)")
+  parser.add_option("--s3-stats-bucket", default="ampcamp-data/wikistats_20090505-0",
+      help="base-name of S3 bucket to copy ampcamp data from. The bucket number " +
+           " will be appended at the end to form the complete s3 key " +
+           " (default: ampcamp-data/wikistats_20090505-0)")
+
+  parser.add_option("--s3-small-bucket", default="ampcamp-data/wikistats_20090505-0",
+      help="base-name of S3 bucket to copy ampcamp data from. The bucket number " +
+           " will be appended at the end to form the complete s3 key " +
+           " (default: ampcamp-data/wikistats_20090505-0)")
+
+  # parser.add_option("-p", "--parallel", type="int", default=1,
+  #     help="Number of launches that will happen in parallel (default: 1)")
+
+  # spark-ec2 options that are just passed through
+  parser.add_option("-s", "--slaves", type="int", default=3,
+      help="Number of slaves to launch (default: 3)")
+  parser.add_option("-k", "--key-pair",
+      help="Key pair to use on instances")
+  parser.add_option("-i", "--identity-file", 
+      help="SSH private key file to use for logging into instances")
+  parser.add_option("-a", "--ami", default="latest",
+      help="Amazon Machine Image ID to use, or 'latest' to use latest " +
+           "availabe mesos AMI, 'standalone' for the latest available " +
+           "standalone AMI (default: latest)")
+
+  (opts, args) = parser.parse_args()
+  if len(args) != 1:
+    parser.print_help()
+    sys.exit(1)
+
+  return (opts, args[0])
+
+def main():
+  (opts, spark_script_path) = parse_args()
+  s3_buckets = range(1, opts.buckets + 1)
+  availability_zones = ["us-east-1b", "us-east-1c", "us-east-1d", "us-east-1a"]
+  subprocesses = []
+
+  for cluster in range(1, opts.clusters+1):
+    # Launch a cluster
+    args = []
+    args.append(spark_script_path) 
+
+    args.append('-k')
+    args.append(opts.key_pair)
+    args.append('-i')
+    args.append(opts.identity_file)
+    
+    args.append('-z')
+    args.append(availability_zones[cluster % len(availability_zones)])
+    
+    args.append('--s3-stats-bucket')
+    args.append(opts.s3_stats_bucket + str(s3_buckets[cluster%len(s3_buckets)]))
+    args.append('--s3-small-bucket')
+    args.append(opts.s3_small_bucket + str(s3_buckets[cluster%len(s3_buckets)]))
+    
+    args.append('--copy')
+    args.append('launch')
+    cluster_name = 'ampcamp' + str(cluster)
+    args.append(cluster_name)
+
+    print "Launching " + cluster_name
+    proc = subprocesses.Popen(args, stdout=open("/tmp/" + cluster_name + ".out", "w"),
+                              stderr=open("/tmp/" + cluster_name + ".err", "w"))
+    subprocesses.append(proc)
+    cluster_names.append(cluster_name)
+ 
+    # Wait for all the parallel launches to finish
+    if (len(subprocesses) == opts.parallel):
+      num_success = 0
+      num_mesos_failed = 0
+      print "Waiting for parallel launches to finish...."
+      # Print out details about clusters we launched 
+      for p in range(0, len(subprocesses)): 
+        subprocesses[p].wait()
+        p_stderr = open("/tmp/" + cluster_names[p] + ".err")
+        p_stdout = open("/tmp/" + cluster_names[p] + ".out")
+        errs = p_stderr.readlines()
+        for err in errs:
+          if err.contains("SUCCESS:"):
+            num_success = num_success + 1
+            parts = err.split() 
+            master_name = parts[len(parts) - 1]
+            print >> stderr, ("INFO: Cluster " + cluster_names[p] + " " + master_name)
+            break
+          elif err.contains("ERROR: mesos-check"):
+            num_mesos_failed = num_mesos_failed + 1
+            break
+
+      if (num_success != len(subprocesses)):
+        print("ERROR: Failed to launch all clusters " + num_mesos_failed + " failed mesos check")
+      del subprocesses[:]
+      del cluster_names[:]
+
+if __name__ == "__main__":
+  logging.basicConfig()
+  main()
